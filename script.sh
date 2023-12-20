4daf5021cca6cdf88505f77e246d41feab45abbb
riya-n <riya.narayan@gmail.com>
2023-12-20 18:01:09 -0500

    fix input validation

--- script.sh
+++ script.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Required inputs.
-if [ -z ${JUNIT_PATHS} ]; then
+if [[ -z ${JUNIT_PATHS} ]]; then
 	echo "Missing junit files"
 	exit 2
 fi
 
-if [ -z ${ORG_URL_SLUG}]; then
+if [[ -z ${ORG_URL_SLUG} ]]; then
 	echo "Missing organization url slug"
 	exit 2
 fi
 
-if [ (-z ${INPUT_TOKEN}) && (-z ${TRUNK_API_TOKEN}) ]; then
+if [[ (-z ${INPUT_TOKEN}) && (-z ${TRUNK_API_TOKEN}) ]]; then
 	echo "Missing trunk api token"
 	exit 2
 fi
-TOKEN=${INPUT_TOKEN:-$TRUNK_API_TOKEN} # Defaults to TRUNK_API_TOKEN env var. 
+TOKEN=${INPUT_TOKEN:-${TRUNK_API_TOKEN}} # Defaults to TRUNK_API_TOKEN env var. 
 
 DRY_RUN=${DRY_RUN:-false} # Defaults to false.
