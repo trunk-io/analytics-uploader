117a3a9a0a5292046b17f350b3f7394e05be3076
riya-n <riya.narayan@gmail.com>
2023-12-20 19:04:07 -0500

    add curl

--- script.sh
+++ script.sh
@@ -19,4 +19,12 @@ if [[ (-z ${INPUT_TOKEN}) && (-z ${TRUNK_API_TOKEN}) ]]; then
 fi
 TOKEN=${INPUT_TOKEN:-${TRUNK_API_TOKEN}} # Defaults to TRUNK_API_TOKEN env var. 
 
-DRY_RUN=${DRY_RUN:-false} # Defaults to false.
+DRY_RUN=${INPUT_DRY_RUN:-false} # Defaults to false.
+
+# CLI.
+API_ADDRESS=${INPUT_API_ADDRESS:-"https://api.trunk.io:5022"}
+API_URL=${API_ADDRESS}/ # TODO: add binary
+
+curl -fsSL --retry 1 --retry-connrefused --connect-timeout 5 "${API_URL}" > ./trunk-analytics-uploader
+
+./trunk-analytics-uploader upload --junit-paths ${JUNIT_PATHS} --org-url-slug ${ORG_URL_SLUG} --token ${TOKEN} --api-address ${API_ADDRESS} --repo-root ${REPO_ROOT} --repo-url ${REPO_URL} --repo-head-sha ${REPO_HEAD_SHA} --repo-head-branch ${REPO_HEAD_BRANCH} --repo-head-commit-epoch ${REPO_HEAD_COMMIT_EPOCH} --custom-tags ${CUSTOM_TAGS} --dry-run ${DRY_RUN}
