ed6a496d854a34249b7265fa38dc116f81d15d78
gnalh <gabe@trunk.io>
2024-03-26 17:10:31 +0000

    init

--- script.sh
+++ script.sh
@@ -37,7 +37,6 @@ if [[ (-z ${INPUT_TOKEN}) && (-z ${TRUNK_API_TOKEN-}) ]]; then
     echo "Missing trunk api token"
     exit 2
 fi
-RUN="${RUN-}"
 REPO_HEAD_BRANCH="${REPO_HEAD_BRANCH-}"
 TAGS="${TAGS-}"
 TOKEN=${INPUT_TOKEN:-${TRUNK_API_TOKEN}} # Defaults to TRUNK_API_TOKEN env var.
@@ -51,7 +50,7 @@ tar -xvzf trunk-analytics-cli.tar.gz
 chmod +x ./trunk-analytics-cli
 set +x
 
-if [[ -z ${RUN} ]]; then
+if [[ $# -eq 0 ]]; then
     ./trunk-analytics-cli upload \
         --junit-paths "${JUNIT_PATHS}" \
         --org-url-slug "${ORG_URL_SLUG}" \
@@ -64,6 +63,5 @@ else
         --org-url-slug "${ORG_URL_SLUG}" \
         --token "${TOKEN}" \
         --repo-head-branch "${REPO_HEAD_BRANCH}" \
-        --tags "${TAGS}" \
-        -- "${RUN}"
+        --tags "${TAGS}" "$@"
 fi
