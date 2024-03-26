46ef593ea7ac0385410bdb5f957f12d655ee88bc
Gabe <78751069+gnalh@users.noreply.github.com>
2024-03-25 17:08:40 -0700

    Update analytics uploader versioning (#17)

--- script.sh
+++ script.sh
@@ -21,25 +21,44 @@ if [[ -z ${ORG_URL_SLUG} ]]; then
     exit 2
 fi
 
+if [[ -z ${CLI_VERSION} ]]; then
+    echo "Missing analytics cli version"
+    exit 2
+fi
+
 if [[ (-z ${INPUT_TOKEN}) && (-z ${TRUNK_API_TOKEN-}) ]]; then
     echo "Missing trunk api token"
     exit 2
 fi
+RUN="${RUN-}"
+REPO_HEAD_BRANCH="${REPO_HEAD_BRANCH-}"
+TAGS="${TAGS-}"
 TOKEN=${INPUT_TOKEN:-${TRUNK_API_TOKEN}} # Defaults to TRUNK_API_TOKEN env var.
 
 # CLI.
 set -x
-if [[ ! (-f ./trunk-analytics-uploader) ]]; then
-    curl -fsSL --retry 3 "https://trunk.io/releases/analytics-cli/latest" -o ./trunk-analytics-uploader
+if [[ ! (-f ./trunk-analytics-cli) ]]; then
+    curl -fsSL --retry 3 "https://github.com/trunk-io/analytics-cli/releases/download/${CLI_VERSION}/trunk-analytics-cli-${CLI_VERSION}-x86_64-unknown-linux-gnu.tar.gz" -o ./trunk-analytics-cli.tar.gz
 fi
-chmod +x ./trunk-analytics-uploader
+tar -xvzf trunk-analytics-cli.tar.gz
+chmod +x ./trunk-analytics-cli
 set +x
 
-./trunk-analytics-uploader upload \
-    --junit-paths "${JUNIT_PATHS}" \
-    --org-url-slug "${ORG_URL_SLUG}" \
-    --token "${TOKEN}" \
-    --repo-head-branch "${REPO_HEAD_BRANCH}" \
-    --tags "${TAGS}"
+if [[ -z ${RUN} ]]; then
+    ./trunk-analytics-cli upload \
+        --junit-paths "${JUNIT_PATHS}" \
+        --org-url-slug "${ORG_URL_SLUG}" \
+        --token "${TOKEN}" \
+        --repo-head-branch "${REPO_HEAD_BRANCH}" \
+        --tags "${TAGS}"
+else
+    ./trunk-analytics-cli test \
+        --junit-paths "${JUNIT_PATHS}" \
+        --org-url-slug "${ORG_URL_SLUG}" \
+        --token "${TOKEN}" \
+        --repo-head-branch "${REPO_HEAD_BRANCH}" \
+        --tags "${TAGS}" \
+        -- "${RUN}"
+fi
 
-rm -rf ./trunk-analytics-uploader
+rm -rf ./trunk-analytics-cli ./trunk-analytics-cli.tar.gz
